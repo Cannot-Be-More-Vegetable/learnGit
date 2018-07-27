f8649064c016c5ffddd9d9f830e63d42a2fac70c
Cannot-Be-More-Vegetable <zhuganjun_software@163.com>
2018-07-27 09:39:53 +0800

    a

--- first.c
+++ first.c
@@ -3,9 +3,6 @@ void main()
 {
 	printf("Hello Git , i'm coming");
 	printf("zhuganjun is handsome");
-<<<<<<< HEAD
-	printf("This is a dev try！That's difference! ");
-=======
 	printf("This is a dev try ！And now i add something");
->>>>>>> feature
+
 }
